ad42b3842af9149e52ce1ffea5d2d43cc03da9ec
Jongwon Park <pjw970220@gmail.com>
2026-03-17 02:44:55 +0900

    feat: metadata 추가

--- src/app/(auth)/oauth2/redirect/page.tsx
+++ src/app/(auth)/oauth2/redirect/page.tsx
@@ -1,5 +1,10 @@
 import { Suspense } from "react";
+import type { Metadata } from "next";
 import OAuthRedirectClient from "./ui/OAuthRedirectClient";
+
+export const metadata: Metadata = {
+  title: "DEKK 로그인",
+};
 
 const OAuthRedirectPage = () => {
   return (
